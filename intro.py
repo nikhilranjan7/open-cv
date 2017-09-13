1ea5b31f28cb21c334203c273f132cf189dde72b
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-09-13 16:15:47 +0530

    Intro complete

--- intro.py
+++ intro.py
@@ -3,4 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = cv2.imread('tree.jpg', cv2.IMREAD_GRAYSCALE)
-jkj
+#IMREAD_GRAYSCALE = 0
+#IMREAD_COLOR = 1
+#IMREAD_UNCHANGED = -1
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.show()
